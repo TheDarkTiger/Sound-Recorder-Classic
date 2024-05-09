b5e450fa9ed2242f22632b3efb62bcf2ab026af4
TheDarkTiger <thedarktiger@hotmail.com>
2024-05-10 00:25:58 +0200

    Moved the GUI to external file

--- Makefile
+++ Makefile
@@ -2,9 +2,11 @@ TARGET=test.exe
 BUILD_DIR=build
 
 IUP=C:/LIBC/iup-3.31_Win64_mingw6_lib
+
 CC=gcc
 # CC=C:/mingw64-13.2.0-rev1/bin/gcc
 CFLAGS =-Os -Wall -pedantic -I$(IUP)/include -Isrc/include
+
 LDFLAGS=-L$(IUP)
 LDLIBS =-liup -liupimglib -lgdi32 -lcomdlg32 -lcomctl32 -luuid -loleaut32 -lole32
 
--- src/main.c
+++ src/main.c
@@ -10,150 +10,167 @@
 #include <iup.h>
 #include "iupdraw.h"
 
-Ihandle* val_position;
-Ihandle* cnv_wavform;
 float global_tmpWaveformIndex = 0.0f;
 
-int mnu_file_exit_cb( Ihandle* self )
+
+/**************************************************************************//**
+ * Placeholder callback
+ * 
+ * @param [in] self   Handle
+ * 
+ * @return Default IUP value
+ *****************************************************************************/
+int placeholder_callback( Ihandle *self )
+{
+	printf( "Placeholder callback\n" );
+	return IUP_DEFAULT;
+}
+
+
+/**************************************************************************//**
+ * Menu file exit callback
+ * 
+ * @param [in] self   Menu file handle
+ * 
+ * @return Termination signal
+ *****************************************************************************/
+int mnu_file_exit_cb( Ihandle *self )
 {
 	// Exits the main loop
 	return IUP_CLOSE;
 }
 
-static int canvas_action( Ihandle* ih )
+
+/**************************************************************************//**
+ * Canvas calback
+ * 
+ * @param [in] ih   Canvas handle
+ * 
+ * @return IUP error
+ *****************************************************************************/
+static int cnv_wavform_cb( Ihandle *ih )
 {
-	IupDrawBegin( ih );
-	IupSetAttribute( ih, "DRAWCOLOR", "128 255 0" );
-	IupDrawLine( ih, 0,18, 112,18 );
+	int error = IUP_CLOSE;
 	
-	int x = 112.0f*global_tmpWaveformIndex;
-	IupDrawLine( ih, x,0, x,35 );
-	IupDrawEnd( ih );
+	if( ih != NULL )
+	{
+		IupDrawBegin( ih );
+		IupSetAttributes( ih, "DRAWCOLOR=\"128 255 0\"" );
+		IupDrawLine( ih, 0,18, 112,18 );
+		
+		int x = 112.0f * global_tmpWaveformIndex;
+		IupDrawLine( ih, x,0, x,35 );
+		IupDrawEnd( ih );
+		
+		error = IUP_DEFAULT;
+	}else{
+		error = IUP_CLOSE;
+	}
 	
-	return IUP_DEFAULT;
+	return error;
 }
 
-int val_position_cb( Ihandle* self, double a )
+
+/**************************************************************************//**
+ * Canvas calback
+ * 
+ * @param [in] ih   Slider handle
+ * @param [in] a    Slider value
+ * 
+ * @return IUP error
+ *****************************************************************************/
+int val_position_cb( Ihandle *ih, double a )
 {
-	char* tmp = IupGetAttribute( val_position, "VALUE" );
-	sscanf( tmp, "%lf", &a );
-	global_tmpWaveformIndex = a;
-	IupUpdate( cnv_wavform );
-	return IUP_DEFAULT;
+	int error = IUP_CLOSE;
+	
+	if( ih != NULL )
+	{
+		char* tmp = IupGetAttribute( IupGetHandle( "val_position" ), "VALUE" );
+		sscanf( tmp, "%lf", &a );
+		global_tmpWaveformIndex = a;
+		IupUpdate( IupGetHandle( "cnv_wavform" ) );
+		
+		char tmpstr[40];
+		sprintf( tmpstr, "TITLE=\"Position:\n%4.2f Sec.\"", a );
+		IupSetAttributes( IupGetHandle( "lbl_position" ), tmpstr );
+		
+		error = IUP_DEFAULT;
+	}else{
+		error = IUP_CLOSE;
+	}
+	
+	return error;
 }
 
+
+// Main
 int main( int argc, char** argv )
 {
 	IupOpen( &argc, &argv );
 	
 	IupImageLibOpen();
 	
+	// Try to load the GUI from the external file
+	char *error = NULL;
+	error = IupLoad( "res/main.led" );
+	if( error )
+	{
+		printf( "%s\n", error );
+		return 1 ;
+	}
 	
-	Ihandle* mnu;
-	Ihandle* mnu_file_exit = IupItem( "Exit", NULL );
-	mnu = IupMenu(
-	IupSubmenu( "File", IupMenu(
-	IupSetAttributes( IupItem( "New", NULL), NULL ),
-	IupSetAttributes( IupItem( "Open...", NULL), NULL ),
-	IupSetAttributes( IupItem( "Save", NULL), NULL ),
-	IupSetAttributes( IupItem( "Save As...", NULL), NULL ),
-	IupSetAttributes( IupItem( "Revert", NULL), "ACTIVE=NO" ),
-	IupSetAttributes( IupItem( "Properties", NULL), NULL ),
-	IupSeparator(),
-	IupSetAttributes( mnu_file_exit, NULL ),
-	NULL) ),
-	IupSubmenu( "Edit", IupMenu(
-	IupSetAttributes( IupItem( "Copy\tCtrl+C", NULL), NULL ),
-	IupSetAttributes( IupItem( "Paste Insert\tCtrl+V", NULL), "ACTIVE=NO" ),
-	IupSetAttributes( IupItem( "Paste Mix", NULL), "ACTIVE=NO" ),
-	IupSeparator(),
-	IupSetAttributes( IupItem( "Insert File...", NULL), NULL ),
-	IupSetAttributes( IupItem( "Mix with File...", NULL), NULL ),
-	IupSeparator(),
-	IupSetAttributes( IupItem( "Delete Before Current Position", NULL), "ACTIVE=NO" ),
-	IupSetAttributes( IupItem( "Delete After Current Position", NULL), "ACTIVE=NO" ),
-	IupSeparator(),
-	IupSetAttributes( IupItem( "Audio Properties", NULL), NULL ),
-	NULL) ),
-	IupSubmenu( "Effects", IupMenu(
-	IupSetAttributes( IupItem( "Increase Volume (by 25%)", NULL), NULL ),
-	IupSetAttributes( IupItem( "Decrease Volume", NULL), NULL ),
-	IupSeparator(),
-	IupSetAttributes( IupItem( "Increase Speed (by 100%)", NULL), NULL ),
-	IupSetAttributes( IupItem( "Decrease Speed", NULL), NULL ),
-	IupSeparator(),
-	IupSetAttributes( IupItem( "Add Echo", NULL), NULL ),
-	IupSetAttributes( IupItem( "Reverse", NULL), NULL ),
-	NULL) ),
-	IupSubmenu( "Help", IupMenu(
-	IupSetAttributes( IupItem( "Help Topics", NULL), NULL ),
-	IupSeparator(),
-	IupSetAttributes( IupItem( "About Sound Recorder Classic", NULL), NULL ),
-	NULL) ),
-	NULL);
-	IupSetHandle( "mnu", mnu );
-	
-	Ihandle* hboxInfos;
-	Ihandle* frmPosition;
-	Ihandle* lblPosition;
-	lblPosition = IupLabel( "Position:\n0.00 Sec." );
-	frmPosition = IupFrame( lblPosition );
-	IupSetAttributes( frmPosition, "SUNKEN=YES, MARGIN=5x3, RASTERSIZE=60x37" );
-	
-	// Ihandle* cnv_wavform;
-	cnv_wavform = IupCanvas(NULL);
-	IupSetAttribute( cnv_wavform, "BGCOLOR", "0 0 0" );
-	IupSetAttribute( cnv_wavform, "DRAWSIZE", "112 35" );
-	IupSetAttribute( cnv_wavform, "EXPAND", "NO" );
-	IupSetAttribute( cnv_wavform, "RASTERSIZE", "114x37" );
-	// IupSetAttribute(cnv_wavform,"CANFOCUS","NO");
-	IupSetCallback( cnv_wavform, "ACTION", (Icallback) canvas_action );
-	IupUpdate( cnv_wavform );
-	
-	Ihandle* frmLength;
-	Ihandle* lblLength;
-	lblLength = IupLabel( "Length:\n0.00 Sec." );
-	frmLength = IupFrame( lblLength );
-	IupSetAttributes( frmLength, "SUNKEN=YES, MARGIN=5x3, RASTERSIZE=60x37" );
-	
-	hboxInfos = IupHbox( frmPosition, cnv_wavform, frmLength );
-	
-	// Ihandle* val_position;
-	val_position = IupVal( "HORIZONTAL" );
-	IupSetAttribute( val_position, "MIN", "0" );
-	IupSetAttribute( val_position, "MAX", "1" );
-	IupSetAttribute( val_position, "VALUE", "0.0" );
-	IupSetAttribute( val_position, "RASTERSIZE", "234x21" );
-	
-	Ihandle* hboxControls;
-	Ihandle* btn_rewind,* btn_fastforward,* btn_play;
-	btn_rewind = IupSetAttributes( IupButton( "", NULL ), "IMAGE=IUP_MediaGoToBegin" );
-	btn_fastforward = IupSetAttributes( IupButton( "", NULL ), "IMAGE=IUP_MediaGoToEnd" );
-	btn_play = IupSetAttributes( IupButton( "", NULL ), "IMAGE=IUP_MediaPlay" );
-	hboxControls = IupHbox( btn_rewind, btn_fastforward, btn_play, IupSetAttributes(IupButton( "", NULL ), "IMAGE=IUP_MediaStop" ), IupSetAttributes(IupButton( "", NULL ), "IMAGE=IUP_MediaRecord" ) );
-	
-	Ihandle* vbox;
-	vbox = IupVbox( hboxInfos, val_position, hboxControls, NULL );
-	
-	IupSetAttribute( vbox, "ALIGNMENT", "ACENTER" );
-	IupSetAttribute( vbox, "GAP", "5" );
-	IupSetAttribute( vbox, "MARGIN", "5x3" );
-	
-	Ihandle* dlg;
-	dlg = IupDialog( vbox );
-	
-	IupSetAttribute( dlg, "MENU", "mnu" );
-	IupSetAttribute( dlg, "TITLE", "Sound - Sound Recorder Classic" );
-	// IupSetAttribute( dlg, "RASTERSIZE", "276x151" );
 	
 	// Registers callbacks
-	IupSetCallback( val_position, "VALUECHANGED_CB", (Icallback) val_position_cb );
-	IupSetCallback( mnu_file_exit, "ACTION", (Icallback) mnu_file_exit_cb );
 	
-	IupShowXY( dlg, IUP_CENTER, IUP_CENTER );
+	// Menu 'File'
+	IupSetCallback( IupGetHandle( "mnu_file_new" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_file_open" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_file_save" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_file_saveAs" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_file_revert" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_file_properties" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_file_exit" ), "ACTION", (Icallback) mnu_file_exit_cb );
 	
-	IupMainLoop();
+	// Menu 'Edit'
+	IupSetCallback( IupGetHandle( "mnu_edit_copy" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_edit_pasteInsert" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_edit_pasteMix" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_edit_insertFile" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_edit_mixWithFile" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_edit_deleteBeforeCurrentPos" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_edit_deleteAfterCurrentPos" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_edit_audioProperties" ), "ACTION", (Icallback) placeholder_callback );
 	
-	IupClose();
-	return EXIT_SUCCESS;
+	// Menu 'Effect'
+	IupSetCallback( IupGetHandle( "mnu_effect_increaseVolume" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_effect_decreaseVolume" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_effect_increaseSpeed" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_effect_decreaseSpeed" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_effect_addEcho" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_effect_reverse" ), "ACTION", (Icallback) placeholder_callback );
+	
+	// Menu 'Help'
+	IupSetCallback( IupGetHandle( "mnu_help_helpTopics" ), "ACTION", (Icallback) placeholder_callback );
+	IupSetCallback( IupGetHandle( "mnu_help_aboutSRC" ), "ACTION", (Icallback) placeholder_callback );
+	
+	// slider
+	IupSetCallback( IupGetHandle( "cnv_wavform" ), "ACTION", (Icallback) cnv_wavform_cb );
+	IupUpdate( IupGetHandle( "cnv_wavform" ) );
+	IupSetCallback( IupGetHandle( "val_position" ), "VALUECHANGED_CB", (Icallback) val_position_cb );
+	
+	
+	// Get the main window handle and run the GUI
+	Ihandle *dlg = IupGetHandle( "dlg" );
+	
+	if( dlg != NULL )
+	{
+		IupShowXY( dlg, IUP_CENTER, IUP_CENTER );
+		
+		IupMainLoop();
+		
+		IupClose();
+		return EXIT_SUCCESS;
+	}else{
+		return 2;
+	}
 }
